5c73f66fe66df99be7412ef12c55dc6633a6f15d
michaelbeam <michaeljbeam@gmail.com>
2014-12-30 14:30:17 -0600

    updated json object to deliver 64bit encoded data field. Updated Readme.md

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 Factom Command Line Interface
 ===
 
-Command line interface for the factom client library.
-[](http://github.com/FactomProject/factom)
+Synopsis
+---
+	factom
+		-c [ChainID]	"hex encoded chainid for the entry"
+		-e [extid]		"external id for the entry"			# -e may be used multiple times
+		-h			"display help message"
+		-s			"path to the factomclient"			# default: localhost:8083
+
+Description
+---
+factom-cli takes data from the command arguments and stdin and constructs a json entry for the factomclient to decode and submit to factom.
+
+Examples
+---
+Submiting arbitrary data to factom:
+	$ echo "Some data to save in factom" | factom-cli -c <my chainid> -e mydata -e somekey
+Submitting a file:
+	$ factom-cli -s "facom.org/demo" -c <my chainid> -e filename <file
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -12,15 +13,17 @@ import (
 const usage = "factom -s [server] -c [chainid] -e [extid] -e [extid2] <data"
 
 type entry struct {
-	ChainID	string
-	ExtIDs	[]string
-	Data	string
+	ChainID string
+	ExtIDs  []string
+	Data    string
 }
 
 type extids []string
+
 func (e *extids) String() string {
 	return fmt.Sprint(*e)
 }
+
 func (e *extids) Set(s string) error {
 	*e = append(*e, s)
 	return nil
@@ -28,31 +31,31 @@ func (e *extids) Set(s string) error {
 
 func main() {
 	var (
-		help = flag.Bool("h", false, usage)
-		cid = flag.String("c", "", "hex encoded chainid for the entry")
+		help   = flag.Bool("h", false, usage)
+		cid    = flag.String("c", "", "hex encoded chainid for the entry")
 		server = flag.String("s", "localhost:8083", "path to the factomclient")
-		eids extids
+		eids   extids
 	)
-	
+
 	flag.Var(&eids, "e", "external id for the entry")
 	flag.Parse()
-	
+
 	if *help {
 		fmt.Println(usage)
 		return
 	}
-	
+
 	data := make([]byte, 1024)
 	n, _ := os.Stdin.Read(data)
 	data = data[:n]
-	
+
 	e := new(entry)
 	e.ChainID = *cid
 	for _, v := range eids {
 		e.ExtIDs = append(e.ExtIDs, string(v))
 	}
-	e.Data = string(data)
-	
+	e.Data = base64.StdEncoding.EncodeToString(data)
+
 	body, err := json.Marshal(e)
 	if err != nil {
 		return
@@ -62,6 +65,6 @@ func main() {
 	if err != nil {
 		return
 	}
-	
+
 	return
 }
