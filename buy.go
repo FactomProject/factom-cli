875f48b69505cbb8118d38368d1a688023a97d7e
michaelbeam <michaeljbeam@gmail.com>
2015-02-20 11:17:43 -0600

    updated buy.go

--- buy.go
+++ buy.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 func buy(args []string) error {
-	return fmt.Errorf("buy called")
+	os.Args = args
+	
+	var (
+		serv = flag.String("s", "localhost:8088", "path to the factomclient")
+	)
+	flag.Parse()
+	
+	if len(args) < 2 {
+		return fmt.Errorf("the ammount of factoids to be transferd must be specified")
+	}
+	amt := args[1]
+	server := "http://" + *serv + "/v1/buycredit"
+	data := url.Values{
+		"to":      {"wallet"},
+		"ammount": {amt},
+	}
+	
+	resp, err := http.PostForm(server, data)
+	if err != nil {
+		return err
+	}
+	p, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	fmt.Println(string(p))
+
+	return nil
 }
