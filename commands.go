bad09666020c32809d8b65b8968de691eba96a75
michaelbeam <michaeljbeam@gmail.com>
2015-02-22 13:37:12 -0600

    added copyright banner to all files

--- commands.go
+++ commands.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2015 FactomProject/FactomCode Systems LLC.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
 package main
 
 import (
