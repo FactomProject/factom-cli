2d006050c2e2f3788b7ca6a892007a5c311c188b
michaelbeam <michaeljbeam@gmail.com>
2015-02-25 12:24:06 -0600

    added copyright notice

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-// Copyright (c) 2015 FactomProject/FactomCode Systems LLC.
-// Use of this source code is governed by an ISC
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
+
 package main
 
 import (
