b5f82571f73b90307b78e44945d7dd48a95221ba
michaelbeam <michaeljbeam@gmail.com>
2015-02-25 14:37:21 -0600

    added newest copyright banner to all .go files

--- bintx.go
+++ bintx.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2015 FactomProject/FactomCode Systems LLC.
-// Use of this source code is governed by an ISC
+// Copyright 2015 Factom Foundation
+// Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 package main
 
