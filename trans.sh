ffaed6b5c4f394a227327eafb677ed3d0bf5b30e
Paul Snow <snow.paul@gmail.com>
2015-06-22 12:34:30 -0500

    script updates... still not perfect

--- trans.sh
+++ trans.sh
@@ -1,13 +1,14 @@
-factom-cli genfactoidaddr bill
-factom-cli genfactoidaddr bob
+factom-cli generateaddress fct bill
+factom-cli generateaddress fct bob
 factom-cli newtransaction PayToBob
 factom-cli balance factoid bill
 factom-cli balance factoid bob
-factom-cli addinput PayToBob bill 1000000
-factom-cli addoutput PayToBob bob 500000
+factom-cli addinput PayToBob bill 10
+factom-cli addoutput PayToBob bob 9
 factom-cli sign PayToBob
 echo "Submit Transaction for Bill to pay Bob"
 factom-cli submit PayToBob
 factom-cli balance factoid bill
 factom-cli balance factoid bob
+factom-cli getaddresses
 
