d8d952416cd3caeb79c087ef24c09ed63e0badfd
Paul Snow <snow.paul@gmail.com>
2015-07-12 16:55:36 -0500

    Improved example.sh, lets you specify the delay

--- example.sh
+++ example.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # example script using factom-cli to write data into factom and get it back
 
+if [[ -z $1 ]]; then
+echo "
+************************************************
+*  Defaulting to 1 minute waits.  But if you
+*  are on a 1 minute block server, you can run:
+*
+*  ./example.sh 10s
+*
+*  Which will only wait 10 seconds.  Or if you
+*  are local, you can specify 0s, which will run
+*  way faster!
+*
+*  Time can be specified like 10x where x can be:
+*  s =seconds  m =minute  h = hours   d = days 
+************************************************"
+
+st=60s
+else
+st=$1
+fi
+
 echo "Using the cli to write new data into Factom"
 
 echo "creating factoid and entry credit addresses"
@@ -13,8 +34,8 @@ factom-cli addinput ecpurchase michael 2
 factom-cli addecoutput ecpurchase app 1.8
 factom-cli sign ecpurchase
 factom-cli submit ecpurchase
-echo "sleep for 1 minute to wait for block creation (production system will have 10 minute blocks)"
-sleep 1m
+echo "sleep for " $st " to wait for block creation (production system will have 10 minute blocks)"
+sleep $st
 factom-cli getaddresses
 
 echo "creating a new chain for my entries"
@@ -22,16 +43,16 @@ factom-cli mkchain -e app1 -e mychain app <<FIRSTENTRY
 Hello Factom!
 FIRSTENTRY
 
-echo "sleep for 1 minute to wait for the chain to be included in the block (procution system will have 10 minute blocks)"
-sleep 1m
+echo "sleep for " $st " to wait for the chain to be included in the block (procution system will have 10 minute blocks)"
+sleep $st
 
 echo "adding another entry to my chain"
 factom-cli put -c 9e54c63c6ccf2f1e7bb6e86a4e026b63c5665dca2b649c1cb407d2e39d7e83f3 -e entry -e 2 app <<ENTRY
 Hello again!
 ENTRY
 
-echo "sleep for 1 minute to wait for the entry to be included in the block (procution system will have 10 minute blocks)"
-sleep 1m
+echo "sleep for " $st " to wait for the entry to be included in the block (procution system will have 10 minute blocks)"
+sleep $st
 
 echo "get the newest entry block for my chain"
 eblock=$(factom-cli get chain 9e54c63c6ccf2f1e7bb6e86a4e026b63c5665dca2b649c1cb407d2e39d7e83f3)
@@ -40,3 +61,4 @@ ehash=$(factom-cli get eblock $eblock | grep EntryHash | awk '{print $2}')
 
 echo "get the entry out of the entry block"
 factom-cli get entry $ehash
+
