617c197b713e6015319a8867cba3ad1e095d4452
JackXLu <jack.xujun.lu@gmail.com>
2015-08-12 14:24:01 -0500

    Added script to create an anchor chain

---
+++ createanchorchain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# example script using factom-cli to write data into factom and get it back
+
+echo "creating the anchor chain"
+factom-cli mkchain -e FactomAnchorChain app <<FIRSTENTRY
+This is the Factom anchor chain, which records the anchors Factom puts on Bitcoin and other networks.
+FIRSTENTRY
+
