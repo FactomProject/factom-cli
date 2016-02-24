e017f0709ec6a06c99249fec9c2502eaf1bd18e3
Michael <michaeljbeam@gmail.com>
2016-02-24 12:46:39 -0600

    moved balance command to addresses.go

--- addresses.go
+++ addresses.go
@@ -12,7 +12,53 @@ import (
 
 	"github.com/FactomProject/cli"
 	"github.com/FactomProject/factom"
+	fct "github.com/FactomProject/factoid"
+	"github.com/FactomProject/fctwallet/Wallet/Utility"
 )
+
+// balance prints the current balance of the specified address
+var balance = func() *fctCmd {
+	cmd := new(fctCmd)
+	cmd.helpMsg = "factom-cli balance ADDRESS"
+	cmd.description = "If this is an EC Address, returns number of Entry Credits. If this is a Factoid Address, returns the Factoid balance."
+	cmd.execFunc = func(args []string) {
+		os.Args = args
+		flag.Parse()
+		args = flag.Args()
+		
+		if len(args) < 1 {
+			fmt.Println(cmd.helpMsg)
+			return
+		}
+		addr := args[0]
+		
+		if strings.HasPrefix(addr, "FA") {
+			if !Utility.IsValidAddress(addr) {
+				fmt.Println("Invalid Factoid Address")
+			}
+			
+			if b, err := factom.FctBalance(addr); err != nil {
+				fmt.Println("Undefined or invalid address")
+			} else {
+				fmt.Println(addr, fct.ConvertDecimal(uint64(b)))
+			}
+		} else if strings.HasPrefix(addr, "EC") {
+			if !Utility.IsValidAddress(addr) {
+				fmt.Println("Invalid EC Address")
+			}
+			
+			if b, err := factom.ECBalance(addr); err != nil {
+				fmt.Println("Undefined or invalid address")
+			} else {
+				fmt.Println(addr, b)
+			}
+		} else {
+			fmt.Println("Invalid address")
+		}
+	}
+	help.Add("balance", cmd)
+	return cmd
+}()
 
 // Generate a new Address
 var generateaddress = func() *fctCmd {
