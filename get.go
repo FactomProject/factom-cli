ddd07090876d2c38620f2c468f58328a89f4b06e
Michael <michaeljbeam@gmail.com>
2016-08-04 14:06:33 -0500

    help message update

--- get.go
+++ get.go
@@ -15,7 +15,7 @@ import (
 
 var get = func() *fctCmd {
 	cmd := new(fctCmd)
-	cmd.helpMsg = "factom-cli get allentries|chainhead|dblock|eblock|entry|firstentry|head|height"
+	cmd.helpMsg = "factom-cli get allentries|alltxs|chainhead|dblock|eblock|entry|firstentry|head|height"
 	cmd.description = "get Block or Entry data from factomd"
 	cmd.execFunc = func(args []string) {
 		os.Args = args
