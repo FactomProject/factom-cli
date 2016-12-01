7684f7c8a9b3eee5d8f7c96f757d29482d5fe213
Patrick Stürmlinger <patrick@mindf.org>
2016-12-01 19:20:54 +0100

    Implemented

---
+++ messages.go
@@ -0,0 +1,74 @@
+// Copyright 2016 Factom Foundation
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/FactomProject/factom"
+)
+
+var signmessage = func() *fctCmd {
+	cmd := new(fctCmd)
+	cmd.helpMsg = "factom-cli signmessage ADDRESS 'message'"
+	cmd.description = "Sign the message with the given Factoid address"
+	cmd.execFunc = func(args []string) {
+		os.Args = args
+		flag.Parse()
+		args = flag.Args()
+
+		if len(args) < 2 {
+			fmt.Println(cmd.helpMsg)
+			return
+		}
+
+		addr := args[0]
+		msg := args[1]
+
+		sig, err := factom.SignMessage(addr, msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(sig)
+		return
+	}
+	help.Add("signmessage", cmd)
+	return cmd
+}()
+
+var verifymessage = func() *fctCmd {
+	cmd := new(fctCmd)
+	cmd.helpMsg = "factom-cli verifymessage ADDRESS 'signature' 'message'"
+	cmd.description = "Verify the signature for the given message and Factoid address"
+	cmd.execFunc = func(args []string) {
+		os.Args = args
+		flag.Parse()
+		args = flag.Args()
+
+		if len(args) < 3 {
+			fmt.Println(cmd.helpMsg)
+			return
+		}
+
+		addr := args[0]
+		sig := args[1]
+		msg := args[2]
+
+		result, err := factom.VerifyMessage(addr, sig, msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(result)
+		return
+	}
+	help.Add("verifymessage", cmd)
+	return cmd
+}()
