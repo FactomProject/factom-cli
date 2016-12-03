cf7c5151f91a1728063e3ef36505f9568588041c
Patrick Stürmlinger <patrick@mindf.org>
2016-12-03 23:11:57 +0100

    Made it working

--- messages.go
+++ messages.go
@@ -8,13 +8,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/FactomProject/factom"
 )
 
 var signmessage = func() *fctCmd {
 	cmd := new(fctCmd)
-	cmd.helpMsg = "factom-cli signmessage ADDRESS 'message'"
+	cmd.helpMsg = "factom-cli signmessage ADDRESS \"message\""
 	cmd.description = "Sign the message with the given Factoid or EntryCredit address"
 	cmd.execFunc = func(args []string) {
 		os.Args = args
@@ -29,13 +30,14 @@ var signmessage = func() *fctCmd {
 		addr := args[0]
 		msg := args[1]
 
-		sig, err := factom.SignMessage(addr, msg)
+		pubKeyPrefixed, sig, err := factom.SignMessage(addr, msg)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		fmt.Println(sig)
+		fmt.Println("Public key: " + pubKeyPrefixed)
+		fmt.Println("Signature: " + sig)
 		return
 	}
 	help.Add("signmessage", cmd)
@@ -44,14 +46,14 @@ var signmessage = func() *fctCmd {
 
 var verifymessage = func() *fctCmd {
 	cmd := new(fctCmd)
-	cmd.helpMsg = "factom-cli verifymessage ADDRESS 'signature' 'message'"
-	cmd.description = "Verify the signature for the given message and Factoid or EntryCredit address"
+	cmd.helpMsg = "factom-cli verifymessage <public key> \"signature\" \"message\""
+	cmd.description = "Verify the signature for the given message and public key"
 	cmd.execFunc = func(args []string) {
 		os.Args = args
 		flag.Parse()
 		args = flag.Args()
 
-		if len(args) < 3 {
+		if len(args) != 3 {
 			fmt.Println(cmd.helpMsg)
 			return
 		}
@@ -60,13 +62,14 @@ var verifymessage = func() *fctCmd {
 		sig := args[1]
 		msg := args[2]
 
-		result, err := factom.VerifyMessage(addr, sig, msg)
+		result, addrString, err := factom.VerifyMessage(addr, sig, msg)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		fmt.Println(result)
+		fmt.Println("Address: " + addrString)
+		fmt.Println("Result: " + strconv.FormatBool(result))
 		return
 	}
 	help.Add("verifymessage", cmd)
