7e2fea3cf61c38a90b956e83340c4540632a2bec
mwanon <matt@mattwhittington.com>
2017-08-17 13:22:12 -0500

    test error

--- byheight_test.go
+++ byheight_test.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"testing"
-)
+import "fmt"
 
 var (
 	_ = fmt.Sprint("testing")
 )
-
-func TestHeights(t *testing.T) {
-	// need to have factomd available to really test
-	// find way to run and look for specific results
-	os.Args[0] = "1"
-	os.Args[1] = "-r"
-
-	testEc := Ecbheight
-
-	fmt.Println(testEc.helpMsg)
-
-	testA := Abheight
-
-	fmt.Println(testA.helpMsg)
-
-	testDB := Dbheight
-
-	fmt.Println(testDB.helpMsg)
-
-	testF := Fbheight
-
-	fmt.Println(testF.helpMsg)
-}
--- util_test.go
+++ util_test.go
@@ -20,28 +20,6 @@ func TestGetFactomdServer(t *testing.T) {
 	}
 }
 
-func Test_extidsASCII_Set(t *testing.T) {
-	type args struct {
-		s string
-	}
-	tests := []struct {
-		name    string
-		e       *extidsASCII
-		args    args
-		wantErr bool
-	}{
-		{name: "test1", e: "HI", {s: "123"}, false},
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.e.Set(tt.args.s); (err != nil) != tt.wantErr {
-				t.Errorf("extidsASCII.Set() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
 func Test_extidsHex_Set(t *testing.T) {
 	type args struct {
 		s string
@@ -168,26 +146,6 @@ func Test_namesHex_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.n.String(); got != tt.want {
 				t.Errorf("namesHex.String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_factoshiToFactoid(t *testing.T) {
-	type args struct {
-		v interface{}
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := factoshiToFactoid(tt.args.v); got != tt.want {
-				t.Errorf("factoshiToFactoid() = %v, want %v", got, tt.want)
 			}
 		})
 	}
