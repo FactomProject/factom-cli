67607130e948ed43100b6e0a99b72dd6d3eea93c
Brian Deery <brian@factom.com>
2018-11-01 19:24:13 -0500

    default to quiet

--- unlock.go
+++ unlock.go
@@ -16,11 +16,11 @@ import (
 // unlockwallet creates a new transaction in the wallet.
 var unlockwallet = func() *fctCmd {
 	cmd := new(fctCmd)
-	cmd.helpMsg = "factom-cli walletpassphrase [-q] \"passphrase\" <seconds-to-unlock>"
-	cmd.description = "Unlock the wallet for some number of seconds; must be an encrypted wallet. -q quiet."
+	cmd.helpMsg = "factom-cli walletpassphrase [-v] \"passphrase\" <seconds-to-unlock>"
+	cmd.description = "Unlock the wallet for some number of seconds; must be an encrypted wallet. -v verbose."
 	cmd.execFunc = func(args []string) {
 		os.Args = args
-		qflag := flag.Bool("q", false, "quiet mode; no output")
+		vflag := flag.Bool("v", false, "verbose mode; print relock time in UTC epoch seconds when the unlock is successful")
 		flag.Parse()
 		args = flag.Args()
 
@@ -43,10 +43,10 @@ var unlockwallet = func() *fctCmd {
 
 		// output
 		switch {
-		// quiet mode; don't print anything
-		case *qflag:
-		default:
+		// verbose mode; print when wallet will re-lock
+		case *vflag:
 			fmt.Println(unix)
+		default:
 		}
 	}
 	help.Add("walletpassphrase", cmd)
