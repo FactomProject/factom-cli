0eacfa4b7313bf505ece6128e3fbe1a7afcec406
danielRicaud <ricaud512@gmail.com>
2019-10-18 12:40:46 -0500

    Cleaner success message returned rather than JSON RPC

--- replaystates.go
+++ replaystates.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -14,6 +15,22 @@ import (
 )
 
 var replaydbstates = func() *fctCmd {
+	type JSONError struct {
+		Code    int         `json:"code"`           // The error code associated with the error type
+		Message string      `json:"message"`        // The error message as a concise single sentence
+		Data    interface{} `json:"data,omitempty"` // Optional data object containing additional information about the error
+	}
+
+	type JSON2Response struct {
+		JSONRPC string      `json:"jsonrpc"` // version string which MUST be "2.0" (version 1.0 didn't have this field)
+		ID      interface{} `json:"id"`      // Unique client defined ID associated with incoming request. It may be a number,
+		// string, or nil. It is used by the remote to formulate a this response object
+		// containing the same ID back to the client. If nil, remote treats request object
+		// as a notification, and that the client does not expect a response object back
+		Error  *JSONError  `json:"error,omitempty"`  // Must be present if called subroutine had an error (mutually exclusive with Result)
+		Result interface{} `json:"result,omitempty"` // Must be present if called subroutine succeeded (mutually exclusive with Error)
+	}
+
 	cmd := new(fctCmd)
 	cmd.helpMsg = "factom-cli replaydbstates STARTHEIGHT ENDHEIGHT"
 	cmd.description = "Emit DBStateMsgs over the LiveFeed API between two specifed block heights"
@@ -28,7 +45,6 @@ var replaydbstates = func() *fctCmd {
 
 		startHeightStr, err := strconv.ParseInt(args[0], 10, 32)
 		if err != nil {
-			// fmt.Println(cmd.helpMsg)
 			errorln(err)
 			return
 		}
@@ -39,7 +55,6 @@ var replaydbstates = func() *fctCmd {
 		if len(args) == 2 {
 			endHeightStr, err := strconv.ParseInt(args[1], 10, 32)
 			if err != nil {
-				// fmt.Println(cmd.helpMsg)
 				errorln(err)
 				return
 			}
@@ -50,7 +65,16 @@ var replaydbstates = func() *fctCmd {
 			errorln(err)
 			return
 		}
-		fmt.Println(res)
+
+		resStr, err := json.Marshal(res)
+		if err != nil {
+			errorln(err)
+			return
+		}
+
+		response := JSON2Response{}
+		json.Unmarshal([]byte(resStr), &response)
+		fmt.Println(response.Result.(interface{}).(map[string]interface{})["message"])
 	}
 	help.Add("replaydbstates", cmd)
 	return cmd
